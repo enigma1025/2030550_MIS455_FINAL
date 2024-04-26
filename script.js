b892072dbdca8127b097fdd6d325729db0871abe
enigma1025 <114823302+enigma1025@users.noreply.github.com>
2024-04-26 21:24:48 +0600

    4th commit

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ function api () {
      document.getElementById("search").value = "";
      document.getElementById("container").textContent = "";
   }
+  function display (data){
+    var container = document.getElementById("container");
+    var maxToShow = Math.min(5, data.meals.length); // Show maximum 5 meals or less
+    
+    for (var a = 0; a < maxToShow; a++) {
+        var newDiv = document.createElement("div");
+        newDiv.classList.add("card", "my-4", "col-md-4");
+        newDiv.innerHTML = `
+            <img class="card-img-top" src="${data.meals[a].strMealThumb}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${data.meals[a].strMeal}</h5>
+                <p class="card-id">Meal ID: ${data.meals[a].idMeal}</p>
+                <p class="card-text">${data.meals[a].strInstructions}</p>
+            </div>
+            <a href="#" class="btn btn-danger m-2">Order</a>
+        `;
+        container.appendChild(newDiv);
+    }
+
+    if
