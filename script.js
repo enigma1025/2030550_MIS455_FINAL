6a40bbcf091bb863da362739ae6b3aa97b3a09d2
enigma1025 <114823302+enigma1025@users.noreply.github.com>
2024-04-26 21:51:04 +0600

    last commit

--- script.js
+++ script.js
@@ -41,3 +41,21 @@ function api () {
         container.appendChild(showMoreBtn);
     }
 }
+function displayAllMeals(data) {
+    var container = document.getElementById("container");
+  
+    for (var a = 5; a < data.meals.length; a++) {
+        var newDiv = document.createElement("div");
+        newDiv.classList.add("card", "my-4", "col-md-4");
+        newDiv.innerHTML = `
+            <img class="card-img-top" src="${data.meals[a].strMealThumb}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${data.meals[a].strMeal}</h5>
+                <p class="card-id">Meal ID: ${data.meals[a].idMeal}</p>
+                <p class="card-text">${data.meals[a].strInstructions}</p>
+            </div>
+            <a href="#" class="btn btn-danger m-2">Order</a>
+        `;
+        container.appendChild(newDiv);
+    }
+  }
